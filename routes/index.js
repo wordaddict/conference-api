6808b8bf7dcc3b08c2e84cb00b95676c147213b4
wordaddict_ <madeyinka@terragonltd.com>
2018-09-17 15:49:35 +0100

    Added http status code

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const { mongoose } = require('../config/config');
 const Response = require('../response/response');
 const talksModel = require('../models/talks');
-const attendeesModel = require('../models/attendees')
+const attendeesModel = require('../models/attendees');
+const httpStatus = require('../constants/http_status');
 
 const router = express.Router();
 
@@ -23,7 +24,7 @@ router.post('/talk', (req, res) => {
     }
     const { title, abstract, room, speaker } = req.body;
     if (!title){
-        const incomplete_data = new Response(400, 'Kindly provide all title for the talk', res, true)
+        const incomplete_data = new Response(httpStatus.BAD_REQUEST, 'Kindly provide all title for the talk', res, true)
         return incomplete_data.res_message();  
     }
     const data = {
@@ -35,12 +36,12 @@ router.post('/talk', (req, res) => {
 
     return talksModel.create(data)
         .then(() => {
-            const savedTalk = new Response(200, 'Talk has been registered successfully', res, false);
+            const savedTalk = new Response(httpStatus.CREATED, 'Talk has been registered successfully', res, false);
             return savedTalk.res_message();
         })
         .catch((err) => {
             console.log('Error from saving talk', err);
-            const unsavedTalk = new Response(404, 'Unable to register Talk', res, true);
+            const unsavedTalk = new Response(httpStatus.NOT_FOUND, 'Unable to register Talk', res, true);
             return unsavedTalk.res_message();
         })
 });
@@ -62,12 +63,12 @@ router.post('/attendee', (req, res) => {
 
     return attendeesModel.create(data)
         .then(() => {
-            const savedAttendee = new Response(200, 'Attendee has been registered successfully', res, false);
+            const savedAttendee = new Response(httpStatus.CREATED, 'Attendee has been registered successfully', res, false);
             return savedAttendee.res_message();
         })
         .catch((err) => {
             console.log('Error from saving Attendee', err);
-            const unsavedAttendee = new Response(404, 'Unable to register attendee', res, true);
+            const unsavedAttendee = new Response(httpStatus.NOT_FOUND, 'Unable to register attendee', res, true);
             return unsavedAttendee.res_message();
         })
 });
@@ -91,18 +92,18 @@ router.post('/conf', (req, res) => {
             console.log('d', data)
             return talksModel.findOneAndUpdate(query, { $push: { attendees : att }} )
                 .then(() => {
-                    const updatedTalk = new Response(200, 'Attendee has been added to talk successfully', res, false);
+                    const updatedTalk = new Response(httpStatus.CREATED, 'Attendee has been added to talk successfully', res, false);
                     return updatedTalk.res_message();
                 })
                 .catch((err) => {
                     console.log('update error', err)
-                    const unUpdatedTalk = new Response(404, 'Unable to add attendee to talk', res, true);
+                    const unUpdatedTalk = new Response(httpStatus.NOT_FOUND, 'Unable to add attendee to talk', res, true);
                     return unUpdatedTalk.res_message();
                 })
         })
         .catch((err) => {
             console.log('Error finding by name', err);
-            const unFoundName = new Response(400, 'Unable to find attendee name', res, true);
+            const unFoundName = new Response(httpStatus.BAD_REQUEST, 'Unable to find attendee name', res, true);
             return unFoundName.res_message();
         })
 });
@@ -113,12 +114,12 @@ router.delete('/talk/:title', (req, res) => {
     const { title } = req.params;
     talksModel.deleteOne({ title: title })
         .then(() => {
-            const removeTalk = new Response(200, 'Talk has been removed successfully', res, false);
+            const removeTalk = new Response(httpStatus.OK, 'Talk has been removed successfully', res, false);
             return removeTalk.res_message();
         })
         .catch((err) => {
             console.log('err from deleting', err);
-            const unremovedTalk = new Response(404, 'Unable to remove talk', res, true);
+            const unremovedTalk = new Response(httpStatus.NOT_FOUND, 'Unable to remove talk', res, true);
             return unremovedTalk.res_message();
         })
 })
